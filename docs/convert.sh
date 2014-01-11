f1d996b37a77ab73195dfa7f12f8804647d723ff
arun-gupta <arun.gupta@gmail.com>
2014-01-11 19:40:32 +0100

    Added placeholder chapters

--- docs/convert.sh
+++ docs/convert.sh
@@ -2,3 +2,6 @@ echo "Converting to HTML ..."
 asciidoctor -a toc -a numbered wildfly-lab.adoc
 echo "done"
 
+#echo "Converting to PDF ..."
+#ruby ~/workspaces/asciidoctor-pdf/bin/asciidoctor-pdf wildfly-lab.adoc
+#echo "done"
