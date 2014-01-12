4ce9cd0e17fd170b27c2bb54f29fededa95eb1d6
arun-gupta <arun.gupta@gmail.com>
2014-01-12 17:07:10 +0100

    another interim commit of work in progress

---
+++ samples/javaee7/src/main/java/org/wildfly/samples/javaee7/EmployeeList.java
@@ -0,0 +1,62 @@
+package org.wildfly.samples.javaee7;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Arun Gupta
+ */
+@WebServlet("/EmployeeList")
+public class EmployeeList extends HttpServlet {
+
+    @Inject
+    EmployeeBean bean;
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>List of Employees</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>List of Employees</h1>");
+        for (Employee e : bean.get()) {
+            out.println(e.getName() + "<br>");
+        }
+        out.println("<br><br>Loaded on: " + Calendar.getInstance().getTime());
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
