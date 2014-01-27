0386316e168e6d2090d6648eb63bd6be4715fb89
arun-gupta <arun.gupta@gmail.com>
2014-01-27 12:43:45 +0530

    generating PDF using asciidoctor-fopub

--- docs/convert.sh
+++ docs/convert.sh
@@ -2,6 +2,15 @@ echo "Converting to HTML ..."
 asciidoctor -a toc -a numbered wildfly-lab.adoc
 echo "done"
 
-echo "Converting to PDF ..."
-ruby ~/workspaces/asciidoctor-pdf/bin/asciidoctor-pdf wildfly-lab.adoc
+echo "Converting to DocBook ..."
+asciidoctor -b docbook -a toc -a numbered -d book wildfly-lab.adoc
 echo "done"
+
+echo "Converting to PDF ..."
+~/tools/asciidoctor-fopub/fopub wildfly-lab.xml
+echo "done"
+
+# Giving error https://github.com/opendevise/asciidoctor-pdf/issues/3
+#echo "Converting to PDF ..."
+#ruby ~/workspaces/asciidoctor-pdf/bin/asciidoctor-pdf wildfly-lab.adoc
+#echo "done"
